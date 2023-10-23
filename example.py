56c00a0af629c1cb8cca6f30f5cc822cbb357ebf
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-10-23 16:41:12 +0300

    PyMem Source Codes
    Added some error messages

--- example.py
+++ example.py
@@ -1,17 +1,14 @@
 
-import os
+import os, sys
 from pymem_snaphot import PyMemSnapshot
 
-print("--------------- PyMem Capture v1 ---------------")
+
 if os.name == "nt":
     name = input("Memory image name : ")
     if name == "" or name is None:
         print("FATAL ERROR : IMAGE CAN NOT GET!")
     else:
-        try:
-            print("--------------- MEMORY SNAPSHOTTING PLEASE WAIT ---------------")
-            PyMemSnapshot.get_memimg_win(str(name))  
-        except Exception as e:
-            print("FATAL ERROR : IMAGE CAN NOT GET\nREASON : " + str(e))
+        PyMemSnapshot.get_memimg_win(str(name))  
 else:
-    print("FATAL ERROR : THAT SYSTEMS ARE NOT SUPPORTING!")
+    print("FATAL ERROR : OS ARE NOT SUPPORTING!")
+    sys.exit(0)
--- pymem_snaphot.py
+++ pymem_snaphot.py
@@ -1,13 +1,18 @@
 import psutil
+from ctypes import WinDLL, create_string_buffer, addressof, windll, c_uint
 
 class PyMemSnapshot:
     def get_memimg_win(name):
-        import ctypes, psutil
-        k32 = ctypes.WinDLL('kernel32')
-        size = int(psutil.virtual_memory().total)
-        buffer = ctypes.create_string_buffer(size)
-        address = ctypes.addressof(buffer)
-        k32.ReadProcessMemory(ctypes.windll.kernel32.GetCurrentProcess(), ctypes.c_uint(address), buffer, ctypes.c_uint(size), None)
-        with open(str(name), "wb") as f:
-            f.write(buffer.raw)
+        try:
+            print("--------------- PyMem Capture v1 ---------------")
+            k32 = WinDLL('kernel32')
+            size = int(psutil.virtual_memory().total)
+            buffer = create_string_buffer(size)
+            address = addressof(buffer)
+            k32.ReadProcessMemory(windll.kernel32.GetCurrentProcess(), c_uint(address), buffer, c_uint(size), None)
+            print(f"Writing {size} memory image {address} to {name}...")
+            with open(str(name), "wb") as f:
+                f.write(buffer.raw)
+        except Exception as e:
+            print("FATAL ERROR : IMAGE CAN NOT GET\nREASON : " + str(e))
 
