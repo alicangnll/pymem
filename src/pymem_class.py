1359059109f707c442fe9d45fc1057b4dc5208c0
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-10-30 05:49:44 +0300

    PyMem Update
    PyMem Update

--- src/pymem_class.py
+++ src/pymem_class.py
@@ -12,7 +12,7 @@ def CTL_CODE(DeviceType, Function, Method, Access):
     
 CTRL_IOCTRL = CTL_CODE(0x22, 0x101, 3, 3) # IOCTL_SET_MODE
 INFO_IOCTRL = CTL_CODE(0x22, 0x103, 3, 3)
-INFO_IOCTRL_DEPRECATED = CTL_CODE(0x22, 0x100, 3, 3)
+INFO_IOCTRL_DEPRECATED = CTL_CODE(0x22, 0x100, 0, 3)
 IOCTL_REVERSE_SEARCH_QUERY = CTL_CODE(0x22, 0x104, 3, 3)
 runs = []
 memsize = c_longlong
@@ -24,6 +24,24 @@ class PyMem:
               ["PfnDataBase", "PsLoadedModuleList", "PsActiveProcessHead"] +
               ["Padding%s" % i for i in range(0xff)] +
               ["NumberOfRuns"])
+
+    def GetInfo(fd):
+        result = win32file.DeviceIoControl(fd, INFO_IOCTRL, b"", 102400, None)
+        fmt_string = "Q" * len(PyMem.FIELDS)
+        memory_parameters = dict(zip(PyMem.FIELDS, struct.unpack_from(fmt_string, result)))
+        for k, v in sorted(memory_parameters.items()):
+            if k.startswith("Pad"):
+                continue
+
+            if not v: continue
+
+            print("%s: \t%#08x (%s)" % (k, v, v))
+
+        print("Memory ranges:")
+        print("Start\t\tEnd\t\tLength")
+
+        for start, length in runs:
+            print("0x%X\t\t0x%X\t\t0x%X" % (start, start+length, length))
 
     def ParseMemoryRuns(fd, buf_size):
         result = win32file.DeviceIoControl(fd, INFO_IOCTRL, b"", buf_size, None)
@@ -49,7 +67,7 @@ class PyMem:
             else:
                 raise RuntimeError("Mode %s not supported" % str(modeset))
             win32file.DeviceIoControl(fd, CTRL_IOCTRL, struct.pack("I", mode), 0, None)
-            return modeset
+            print(f"Mode has been set to {modeset}")
         except Exception as e:
             return str(e)
         
@@ -104,9 +122,9 @@ class PyMem:
         if device_handle is True:
             print("Error: open device failed.\n")
         else:
-            status = PyMem.SetMode(device_handle)
+            PyMem.SetMode(device_handle)
             PyMem.ParseMemoryRuns(device_handle, memsize)
-            print(f"Mode has been set to {status}")
+            PyMem.GetInfo(device_handle)
             with open(filename + ".aff", "wb") as f:
                 mem_addr = 0
                 while mem_addr < memsize:
@@ -114,10 +132,10 @@ class PyMem:
                     data = win32file.ReadFile(device_handle, memsize)[1] # Reading memory...
                     f.write(data) # Writing to file
                     mem_addr += memsize
-                    offset_in_mb = mem_addr / 1024/1024
+                    offset_in_mb = mem_addr / 1024 / 1024
                     if not offset_in_mb % 50:
                         sys.stdout.write("\n%04dMB\t" % offset_in_mb)
                     sys.stdout.flush()
-                    print(f"Dumped {mem_addr} / {memsize} bytes ({mem_addr * 100 / memsize:.2f}%)")
+                    print(f"Dumped {mem_addr} / {memsize} bytes and {offset_in_mb} MB/offset ({mem_addr * 100 / memsize:.2f}%)")
                 f.close()
                 win32file.CloseHandle(device_handle)
